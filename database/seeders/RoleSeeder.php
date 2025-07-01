9a9dfe21db376ad019b924e41295f26d0ac6917f
Pablo Ingegnieri <pabloingegnieri1@gmail.com>
2025-06-30 21:56:51 -0300

    Implementation of user roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $agent_role = Role::query()->updateOrCreate(['name' => Role::AGENT]);
+        $user_role = Role::query()->updateOrCreate(['name' => Role::USER]);
+    }
+}
